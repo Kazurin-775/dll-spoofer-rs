d45800e89deade7cc85549b6909ab6826b8e6c1b
Kazurin Nanako <71819243+Kazurin-775@users.noreply.github.com>
2021-11-11 22:07:40 +0800

    Support newer specs of `asm` and `naked_functions`
    With the update of corresponding RFCs, naked functions can only contain
    one `asm!` with `options(noreturn)` now, and we can only use `sym` and
    `const` operands in the `asm!` statement.
    
    Currently, the only solution to this seems to be doing all the pointer
    arithmetics manually (pointer
    arithmetics does not work in const functions).
    Also, there is a quirk in the assembler where `[rip + disp]` must be
    used instead of `[disp]` in IA-64 mode.

--- src/proxy_funcs.rs
+++ src/proxy_funcs.rs
@@ -5,8 +5,25 @@ macro_rules! proxy_func {
         #[no_mangle]
         #[naked]
         #[allow(non_snake_case)]
-        pub unsafe fn $func_name() {
-            asm!("jmp rax", in("rax") config::FUNCTIONS[$index]);
+        pub unsafe extern fn $func_name() {
+            // Since Rust only allows using `sym` and `const` in a naked
+            // function now, we have to manually compute the address of
+            // the function pointer, using the address of `config::FUNCTIONS`
+            // and the array index.
+            //
+            // When this line is fed into the assembler, it becomes sth like
+            // `jmp [rip + some_mangled_name + some_number]`.
+            // This provides sufficient information for the assembler to do
+            // the math for us.
+            //
+            // The old-fashioned way of using `&config::FUNCTIONS[$index]`
+            // does not work anymore.
+            asm!(
+                "jmp [rip + {base} + {offset}]",
+                base = sym config::FUNCTIONS,
+                offset = const (std::mem::size_of::<*const ()>() * $index),
+                options(noreturn),
+            );
         }
     };
 }
